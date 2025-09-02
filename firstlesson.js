1962e3bf41bca7dba11a34796a654b94ef85114f
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-02 15:48:24 +0300

    Add files via upload

---
+++ firstlesson.js
@@ -0,0 +1,48 @@
+// // json object type
+// let user = {
+//     "name": "berkant",
+//     "lastname": "gunes",
+//     "age": 29,
+//     "location": {
+//         "city": "istanbul",
+//         "district": "kadikoy"
+//     },
+//     "hobbies": ["coding","learning"]
+// }
+
+// let values;
+
+// values = user.location.district; //user["location"]["district"];
+// hobby = user.hobbies[0];
+
+// console.log(hobby);    CTRL K C bütün satirlari commente aldi.
+
+// let numbers = [1,5,9,3,6,7];
+
+// for(let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// function greetings(msg) { console.log(msg); }
+
+// greetings("i am a blockchain developer from istanbul");
+
+// function ageCalculate(birtdayDate) {
+//     return (new Date().getFullYear() - birtdayDate);
+// }
+
+// ageCalculate(1996);
+
+// function retirementCalculate(birtdayDate, name) {
+//     let age = ageCalculate(birtdayDate);
+//     let rest_of_years = 65 - age;
+
+//     if(rest_of_years > 0) {
+//         console.log(`${name}, rest of years ${rest_of_years} to retirement`);
+//     } else {
+//         console.log("you've already retired");
+//     }
+// }
+
+// retirementCalculate(1996, "Berkant");
+
