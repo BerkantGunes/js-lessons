5eec642faeda2ed5a33887ed4e2cc30416584cbc
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-09 08:39:09 +0300

    Add files via upload

---
+++ categories.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const categories = () => {
+  return (
+    <ul className='flex gap-4 md:flex-col text-lg'>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>All</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Seasional Drinks</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Espresso Drinks</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Refresha Drinks</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Tea & Other Beverages</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Cakes & Cookies</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Sandwiches</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Sandwiches</span>
+        </li>
+        <li className='bg-green-600 px-6 py-10 text-white cursor-pointer hover:bg-orange-500 transition-all text-center min-w-[145px]'>
+            <span>Sandwiches</span>
+        </li>
+    </ul>
+  )
+}
+
+export default categories
