0c884ca8483769cf9075bbe4fc68fcc71e3f669a
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-10 10:00:56 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,25 +1,15 @@
-import Header from "./components/Header/Header";
-import Categories from "./components/categories/categories";
-import Products from "./components/products/Products";
-import CartTotal from "./components/cart/CartTotal";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <>
-      <Header />
-
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-20">
-        <div className="categories overflow-auto max-h-[calc(85vh_-_10px)] md:pb-6">
-          <Categories />
-        </div>
-        <div className="products flex-[8] pl-9 pr-8 max-h-[calc(85vh_-_10px)] overflow-y-auto pb-10">
-          <Products />
-        </div>
-        <div className="card-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] flex-col justify-between border">
-          <CartTotal />
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element= {<HomePage />} />
+        <Route path="/cart" element= {<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
