2542b46d2c33c24aafeddd77ab36b7127ae7c6d4
Berkant Gunes <gunesberkant96@gmail.com>
2025-09-10 09:59:31 +0300

    Add files via upload

---
+++ Products.jsx
@@ -0,0 +1,91 @@
+
+
+const Products = () => {
+  return (
+    <div className="products-wrapper grid grid-cols-card gap-4">
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+            <div className="product-img">
+                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">Espresso </span>
+                <span>7 $</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Products
